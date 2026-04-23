6ca3daf6d06438f4d276a0e8a904d0c3f68dad6d
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-23 18:49:37 +0300

    Create ModEntry.cs

---
+++ LuuaGorevPanosu/ModEntry.cs
@@ -0,0 +1,32 @@
+using StardewValley;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using Microsoft.Xna.Framework;
+
+namespace LuuaGorevPanosu
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (e.Button.IsActionButton())
+            {
+                Vector2 tile = e.Cursor.GrabTile;
+                GameLocation location = Game1.currentLocation;
+
+                if (location.Name == "Town" && tile.X == 11 && tile.Y == 23)
+                {
+                    Game1.drawObjectDialogue("Buraya yakinda gorevler eklenecek!");
+                }
+            }
+        }
+    }
+}
